2a545adf0c52ad64c459db83b39bf66cbeb3c0c1
Daniel Wiltshire <i7754737@bournemouth.ac.uk>
2018-03-03 17:45:40 +0000

    Basic HTML skeleton created

---
+++ static/js/custom.js
@@ -0,0 +1,20 @@
+var categoryContent = [
+    { category: 'Applications', title: 'Graylog' },
+    { category: 'Applications', title: 'Foreman' },
+    { category: 'Applications', title: 'Active Directory' },
+    { category: 'Servers', title: 'NYC-GRAY01' },
+    { category: 'Servers', title: 'NYC-FM01' },
+    { category: 'Servers', title: 'NYC-DC01' },
+  ];
+
+$('.ui.search')
+.search({
+  type: 'category',
+  source: categoryContent
+})
+;
+
+$('.ui.dropdown')
+.dropdown({
+    fields: categoryContent,
+});
